51536327ff96ae420e752c2764e81fa5aaa727e2
igromantia <igromantia@yandex.ru>
2017-01-27 22:35:45 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+﻿<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+
+<?php
+include("header.php");
+include("content.php");
+include("sidebar.php");
+include("footer.php");
+?>
+
+</body>
+</html>
